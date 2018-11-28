36b219f912bd91657cae6db94651a12ddb2aa0f1
parag-gurjar02 <44253724+parag-gurjar02@users.noreply.github.com>
2018-11-28 09:49:30 +0530

    Update flask_server.py

--- flaskwebapp/flask_server.py
+++ flaskwebapp/flask_server.py
@@ -42,4 +42,5 @@ def healthy():
     return "Healthy"
 
 if __name__ == '__main__':
-   app.run(host='127.0.0.1',port=5000,debug = True)
+   #app.run(host='127.0.0.1',port=5000,debug = True)
+   app.run(host='0.0.0.0') # Ignore, Development server
